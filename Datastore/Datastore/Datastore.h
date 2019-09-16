5e18d1fe38f07c9aa2528e9d7576cb16fc7122d7
Sam Deane <sam@elegantchaos.com>
2019-09-16 10:12:17 +0100

    first commit

---
+++ Datastore/Datastore/Datastore.h
@@ -0,0 +1,19 @@
+//
+//  Datastore.h
+//  Datastore
+//
+//  Created by Developer on 16/09/2019.
+//  Copyright © 2019 Developer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Datastore.
+FOUNDATION_EXPORT double DatastoreVersionNumber;
+
+//! Project version string for Datastore.
+FOUNDATION_EXPORT const unsigned char DatastoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Datastore/PublicHeader.h>
+
+
